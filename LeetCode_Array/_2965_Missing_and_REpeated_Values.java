a7d4cb45273407069d52d1d0fa86f3f1a8f5fccf
Chetna Soni <143208831+chetnasoni1310@users.noreply.github.com>
2025-03-06 18:36:32 +0530

    updated

---
+++ LeetCode_Array/_2965_Missing_and_REpeated_Values.java
@@ -0,0 +1,35 @@
+public class _2965_Missing_and_REpeated_Values {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        
+        /*Key observation:
+        1. USe a tree map for storing the values in ascending order 
+        2.Then choose whoose freq is 2
+        3. Then traverse the tree map for missing part  */
+
+        int n=grid.length;
+        int totalNum=n*n;
+
+        TreeMap<Integer , Integer> map=new TreeMap<>();
+        int[]result=new int[2];
+        for(int[] row : grid)
+        {
+            for(int num : row)
+            {
+                if(map.containsKey(num))
+                {
+                    result[0]=num;
+                }
+                map.put(num , map.getOrDefault(num , 0)+1);
+            }
+        }
+       
+       
+        for (int i = 1; i <= totalNum; i++) {
+        if (!map.containsKey(i)) {
+            result[1] = i; // The missing number
+            break;
+        }
+        }
+        return result;
+    }
+}
